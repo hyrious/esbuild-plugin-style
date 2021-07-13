1109a6c24c206dd03d9218f54c593177e63ac322
hyrious <hyrious@outlook.com>
2021-07-13 13:15:41 +0800

    refactor: try to set `path` to relative path

--- index.ts
+++ index.ts
@@ -17,16 +17,15 @@ export interface StylePluginOptions {
 
 // https://github.com/evanw/esbuild/issues/20#issuecomment-802269745
 export function style({ minify = true, charset = "utf8" }: StylePluginOptions = {}): Plugin {
-  const options: BuildOptions = { logLevel: "silent", bundle: true, charset, minify };
-
   return {
     name: "style",
     setup({ onResolve, onLoad }) {
+      const cwd = process.cwd();
+      const opt: BuildOptions = { logLevel: "silent", bundle: true, write: false, charset, minify };
+
       onResolve({ filter: /\.css$/ }, args => {
-        if (args.namespace === "style-stub") {
-          return { path: args.path, namespace: "style-content" };
-        }
-        return { path: path.join(args.resolveDir, args.path), namespace: "style-stub" };
+        if (args.namespace === "style-stub") return { path: args.path, namespace: "style-content" };
+        return { path: path.relative(cwd, path.join(args.resolveDir, args.path)), namespace: "style-stub" };
       });
 
       onResolve({ filter: /^__style_helper__$/ }, args => ({
@@ -47,19 +46,17 @@ export function style({ minify = true, charset = "utf8" }: StylePluginOptions = 
       }));
 
       onLoad({ filter: /.*/, namespace: "style-stub" }, async args => ({
-        contents: `import { injectStyle } from "__style_helper__"
-        import css from ${JSON.stringify(args.path)}
-        injectStyle(css)`,
+        contents: `
+          import { injectStyle } from "__style_helper__"
+          import css from ${JSON.stringify(args.path)}
+          injectStyle(css)
+        `,
       }));
 
       onLoad({ filter: /.*/, namespace: "style-content" }, async args => {
-        const result = await esbuild.build({ entryPoints: [args.path], ...options, write: false });
-        return {
-          errors: result.errors,
-          warnings: result.warnings,
-          contents: result.outputFiles[0].text,
-          loader: "text",
-        };
+        const options = { entryPoints: [args.path], ...opt };
+        const { errors, warnings, outputFiles } = await esbuild.build(options);
+        return { errors, warnings, contents: outputFiles![0].text, loader: "text" };
       });
     },
   };
